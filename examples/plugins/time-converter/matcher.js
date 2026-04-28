ecef9f3333ccbd7c3fdec109ade5d6611bb2102d
hanjiedeng <hanjiedeng@trip.com>
2026-04-28 11:37:33 +0800

    fix: tighten time converter matcher

--- examples/plugins/time-converter/matcher.js
+++ examples/plugins/time-converter/matcher.js
@@ -9,6 +9,43 @@ const LOCAL_NUMERIC_RE =
   /^(\d{4})([-/])(\d{1,2})\2(\d{1,2})(?:[ T](\d{1,2})(?::(\d{1,2})(?::(\d{1,2})(?:\.(\d{1,3}))?)?)?)?$/
 const ISO_DATE_PARTS_RE =
   /^(\d{4})-(\d{2})-(\d{2})(?:[T ](\d{2})(?::(\d{2})(?::(\d{2})(?:\.(\d{1,3}))?)?)?)?/
+const MONTH_NAME_PATTERN =
+  "(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t(?:ember)?)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)"
+const MONTH_NAME_RE = new RegExp(`\\b${MONTH_NAME_PATTERN}\\b`, "i")
+const MONTH_FIRST_DATE_RE = new RegExp(
+  `\\b(${MONTH_NAME_PATTERN})\\s+(\\d{1,2})(?:st|nd|rd|th)?(?:,)?\\s+(\\d{4})\\b`,
+  "i"
+)
+const DAY_FIRST_DATE_RE = new RegExp(
+  `\\b(\\d{1,2})(?:st|nd|rd|th)?\\s+(${MONTH_NAME_PATTERN})(?:,)?\\s+(\\d{4})\\b`,
+  "i"
+)
+const MONTH_BY_NAME = {
+  jan: 1,
+  january: 1,
+  feb: 2,
+  february: 2,
+  mar: 3,
+  march: 3,
+  apr: 4,
+  april: 4,
+  may: 5,
+  jun: 6,
+  june: 6,
+  jul: 7,
+  july: 7,
+  aug: 8,
+  august: 8,
+  sep: 9,
+  sept: 9,
+  september: 9,
+  oct: 10,
+  october: 10,
+  nov: 11,
+  november: 11,
+  dec: 12,
+  december: 12,
+}
 
 function parseSupportedTime(value) {
   if (!value) {
@@ -24,11 +61,28 @@ function parseSupportedTime(value) {
     return dateFromLocalMatch(localMatch)
   }
 
+  if (MONTH_NAME_RE.test(value)) {
+    return parseNamedMonthTime(value)
+  }
+
   if (!looksLikeFormattedTime(value)) {
     return null
   }
 
   if (!hasValidIsoDateParts(value)) {
+    return null
+  }
+
+  const date = new Date(value)
+  return isValidDate(date) ? date : null
+}
+
+function parseNamedMonthTime(value) {
+  const dateParts = getNamedMonthDateParts(value)
+  if (
+    !dateParts ||
+    !isValidDateParts(dateParts.year, dateParts.month, dateParts.day)
+  ) {
     return null
   }
 
@@ -97,11 +151,33 @@ function dateFromLocalMatch(matchResult) {
 }
 
 function looksLikeFormattedTime(value) {
-  return (
-    /[A-Za-z]/.test(value) ||
-    /\d{4}[-/]\d{1,2}[-/]\d{1,2}/.test(value) ||
-    /\d{1,2}\s+[A-Za-z]{3,}/.test(value)
-  )
+  return /\d{4}[-/]\d{1,2}[-/]\d{1,2}/.test(value)
+}
+
+function getNamedMonthDateParts(value) {
+  const monthFirstMatch = MONTH_FIRST_DATE_RE.exec(value)
+  if (monthFirstMatch) {
+    return {
+      year: Number(monthFirstMatch[3]),
+      month: monthNumberFromName(monthFirstMatch[1]),
+      day: Number(monthFirstMatch[2]),
+    }
+  }
+
+  const dayFirstMatch = DAY_FIRST_DATE_RE.exec(value)
+  if (dayFirstMatch) {
+    return {
+      year: Number(dayFirstMatch[3]),
+      month: monthNumberFromName(dayFirstMatch[2]),
+      day: Number(dayFirstMatch[1]),
+    }
+  }
+
+  return null
+}
+
+function monthNumberFromName(value) {
+  return MONTH_BY_NAME[value.toLowerCase()] ?? null
 }
 
 function hasValidIsoDateParts(value) {
